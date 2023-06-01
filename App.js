2ba3b9d1d8fc1cebf2bbdce98cc459e04d7139e4
Abdullah Zeb <abdullahzeb0301@gmail.com>
2023-06-02 00:52:43 +0500

    first commit

--- App.js
+++ App.js
@@ -1,41 +1,40 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import YourAiAssistant from './src/screens/onBoarding/YourAiAssistant';
-import HelpUsGrow from './src/screens/onBoarding/HelpUsGrow';
-import EnableNotifications from './src/screens/onBoarding/EnableNotifications';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import YourAiAssistant from "./src/screens/onBoarding/YourAiAssistant";
+import HelpUsGrow from "./src/screens/onBoarding/HelpUsGrow";
+import EnableNotifications from "./src/screens/onBoarding/EnableNotifications";
 
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createStackNavigator();
 
 function OnBoarding() {
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="YourAiAssistant" component={YourAiAssistant} />
-        <Stack.Screen name="HelpUsGrow" component={HelpUsGrow} />
-        <Stack.Screen name="EnableNotifications" component={EnableNotifications} />
+        <Stack.Screen name="Your Ai Assistant" component={YourAiAssistant} />
+        <Stack.Screen name="Help Us Grow" component={HelpUsGrow} />
+        <Stack.Screen
+          name="Enable Notifications"
+          component={EnableNotifications}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-
+  );
 }
 
 export default function App() {
-  return (
-    <OnBoarding/>
-  );
+  return <OnBoarding />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
