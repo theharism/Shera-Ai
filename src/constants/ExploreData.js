039b0eedf018fab834290fa7dc7c28e05219da77
theharism <chharis9999@gmail.com>
2023-06-03 06:53:57 -0700

    Configured Explore Home

--- src/constants/ExploreData.js
+++ src/constants/ExploreData.js
@@ -1,8 +1,8 @@
 export const Content = [
     {   id:1,
         icon:require('../../assets/camera.svg'),
-        title:'Summarize (TL;DR)',
-        subTitle:'Extract key points from long text.'
+        title:'Write a Paragraph',
+        subTitle:'Generate well-written paragraphs on any given subject.'
     },
     {   id:2,
         icon:require('../../assets/camera.svg'),
@@ -11,17 +11,196 @@ export const Content = [
     },
     {   id:3,
         icon:require('../../assets/camera.svg'),
-        title:'Summarize (TL;DR)',
-        subTitle:'Extract key points from long text.'
+        title:'Improve',
+        subTitle:'Rewrite content to make it better and more reliable.'
     },
     {   id:4,
         icon:require('../../assets/camera.svg'),
-        title:'Summarize (TL;DR)',
-        subTitle:'Generate lyrics of a song for any music genre.'
+        title:'Translate',
+        subTitle:'Translate any text from one language into another.'
+    }
+]
+
+export const Artist = [
+    {   id:1,
+        icon:require('../../assets/camera.svg'),
+        title:'Lyrics',
+        subTitle:''
+    },
+    {   id:2,
+        icon:require('../../assets/camera.svg'),
+        title:'Poem',
+        subTitle:''
+    },
+    {   id:3,
+        icon:require('../../assets/camera.svg'),
+        title:'Story',
+        subTitle:''
+    },
+    {   id:4,
+        icon:require('../../assets/camera.svg'),
+        title:'Short Movie',
+        subTitle:''
+    }
+]
+
+export const Business = [
+    {   id:1,
+        icon:require('../../assets/camera.svg'),
+        title:'Company Bio',
+        subTitle:''
+    },
+    {   id:2,
+        icon:require('../../assets/camera.svg'),
+        title:'Name Generator',
+        subTitle:''
+    },
+    {   id:3,
+        icon:require('../../assets/camera.svg'),
+        title:'Slogan',
+        subTitle:''
+    },
+    {   id:4,
+        icon:require('../../assets/camera.svg'),
+        title:'Advertisements',
+        subTitle:''
     },
     {   id:5,
         icon:require('../../assets/camera.svg'),
-        title:'Summarize (TL;DR)',
-        subTitle:'Extract key points from long text.'
+        title:'Job Post',
+        subTitle:''
+    }
+]
+
+export const Personal = [
+    {   id:1,
+        icon:require('../../assets/camera.svg'),
+        title:'Birthday',
+        subTitle:''
+    },
+    {   id:2,
+        icon:require('../../assets/camera.svg'),
+        title:'Apology',
+        subTitle:''
+    },
+    {   id:3,
+        icon:require('../../assets/camera.svg'),
+        title:'Invitation',
+        subTitle:''
+    },
+    {   id:4,
+        icon:require('../../assets/camera.svg'),
+        title:'Pick Up Line',
+        subTitle:''
+    },
+    {   id:5,
+        icon:require('../../assets/camera.svg'),
+        title:'Speech',
+        subTitle:''
+    }
+]
+
+export const Email = [
+    {   id:1,
+        icon:require('../../assets/camera.svg'),
+        title:'Email',
+        subTitle:''
+    },
+    {   id:2,
+        icon:require('../../assets/camera.svg'),
+        title:'Write Email Subject',
+        subTitle:''
+    },
+    {   id:3,
+        icon:require('../../assets/camera.svg'),
+        title:'Improve Email',
+        subTitle:''
+    }
+]
+
+export const Social = [
+    {   id:1,
+        icon:require('../../assets/camera.svg'),
+        title:'Tweet',
+        subTitle:''
+    },
+    {   id:2,
+        icon:require('../../assets/camera.svg'),
+        title:'Turn Into Tweet',
+        subTitle:''
+    },
+    {   id:3,
+        icon:require('../../assets/camera.svg'),
+        title:'LinkedIn Post',
+        subTitle:''
+    },
+    {   id:4,
+        icon:require('../../assets/camera.svg'),
+        title:'Instagram Caption',
+        subTitle:''
+    },
+    {   id:5,
+        icon:require('../../assets/camera.svg'),
+        title:'TikTok Caption',
+        subTitle:''
+    },
+    {   id:6,
+        icon:require('../../assets/camera.svg'),
+        title:'Viral Videos Ideas',
+        subTitle:''
+    }
+]
+
+export const Code = [
+    {   id:1,
+        icon:require('../../assets/camera.svg'),
+        title:'Write Code',
+        subTitle:''
+    },
+    {   id:2,
+        icon:require('../../assets/camera.svg'),
+        title:'Explain Code',
+        subTitle:''
+    }
+]
+
+export const Food = [
+    {   id:1,
+        icon:require('../../assets/camera.svg'),
+        title:'Recipe',
+        subTitle:''
+    },
+    {   id:2,
+        icon:require('../../assets/camera.svg'),
+        title:'Diet Plan',
+        subTitle:''
+    }
+]
+
+export const Entertainment = [
+    {   id:1,
+        icon:require('../../assets/camera.svg'),
+        title:'Movies to Emoji',
+        subTitle:''
+    },
+    {   id:2,
+        icon:require('../../assets/camera.svg'),
+        title:'Tell Joke',
+        subTitle:''
+    },
+    {   id:3,
+        icon:require('../../assets/camera.svg'),
+        title:'Complex Sentence',
+        subTitle:''
+    },
+    {   id:4,
+        icon:require('../../assets/camera.svg'),
+        title:'Create Conversation',
+        subTitle:''
+    },
+    {   id:5,
+        icon:require('../../assets/camera.svg'),
+        title:'Made Up Words',
+        subTitle:''
     }
 ]
--- src/screens/explore/ExploreHome.js
+++ src/screens/explore/ExploreHome.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS } from "../../constants/COLORS";
-import { Content } from "../../constants/ExploreData";
+import { Artist, Business, Code, Content, Email, Entertainment, Food, Personal, Social } from "../../constants/ExploreData";
 import ExploreList from "../../components/ExploreList";
 import { ScrollView } from "react-native-gesture-handler";
 
@@ -12,13 +12,14 @@ const ExploreHome = () => {
       <View style={styles.container}>
         <ScrollView>
           <ExploreList title="Content" data={Content} />
-          <ExploreList title="Content" data={Content} />
-          <ExploreList title="Content" data={Content} />
-          <ExploreList title="Content" data={Content} />
-          <ExploreList title="Content" data={Content} />
-          <ExploreList title="Content" data={Content} />
-          <ExploreList title="Content" data={Content} />
-          <ExploreList title="Content" data={Content} />
+          <ExploreList title="Artist" data={Artist} />
+          <ExploreList title="Business" data={Business} />
+          <ExploreList title="Personal" data={Personal} />
+          <ExploreList title="Email" data={Email} />
+          <ExploreList title="Social" data={Social} />
+          <ExploreList title="Code" data={Code} />
+          <ExploreList title="Food" data={Food} />
+          <ExploreList title="Entertainment" data={Entertainment} />
         </ScrollView>
       </View>
     </SafeAreaView>
