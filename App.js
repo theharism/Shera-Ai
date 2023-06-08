c575231d3316871eb801f0f1978556b0c8632575
Abdullah Zeb <abdullahzeb0301@gmail.com>
2023-06-08 23:18:45 +0500

    Second Commit, Added BottomSheet after great surr khapayi of 1 week :)

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import "react-native-gesture-handler";
+import "react-native-reanimated";
 
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useCallback, useRef, useMemo, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
-import { StatusBar } from "expo-status-bar";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -15,14 +25,15 @@ import ChatHome from "./src/screens/chat/ChatHome";
 import ExploreHome from "./src/screens/explore/ExploreHome";
 import RecentsHome from "./src/screens/recents/RecentsHome";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { Button, Modal, Portal, PaperProvider } from "react-native-paper";
-import React,{useState} from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import ChatScreen from "./src/screens/chat/ChatScreen";
+import BottomSheet, { useBottomSheet } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const ScreenHeight = Dimensions.get("window").height;
 
 function OnBoarding({ setFlag }) {
   return (
@@ -53,7 +64,7 @@ const customHeaderLeft = (prop) => (
     style={{ width: 35, height: 35, left: 25, bottom: 2 }}
   />
 );
-const customHeaderRight = ({ showModal }) => (
+const customHeaderRight = ({ showModal, showBottomSheet }) => (
   <View style={{ flexDirection: "row" }}>
     <Button
       icon="star"
@@ -70,6 +81,7 @@ const customHeaderRight = ({ showModal }) => (
       size={40}
       style={{ marginHorizontal: 10 }}
       color="#c0c0c0"
+      onPress={showBottomSheet}
     />
   </View>
 );
@@ -135,52 +147,111 @@ export default function App() {
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
   const containerStyle = { backgroundColor: "white", padding: 20 };
+  const bscontainerStyle = {
+    color: "black",
+  };
 
   const [flag, setFlag] = useState(false);
 
+  const showBottomSheet = () => {
+    console.log("Setbflag now true");
+    setbFlag(true);
+    bottomSheetModalRef.current?.snapToIndex(0);
+  };
+
+  const [bflag, setbFlag] = useState(false);
+
+  const bottomSheetModalRef = useRef(null);
+  const snapPoints = useMemo(() => ["99%"], []);
+
+  const handleSheetChanges = useCallback((index) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
   return (
     <PaperProvider style={{ flex: 1 }}>
-      <NavigationContainer>
-        {flag ? (
-          <>
-            <Portal>
-              <Modal
-                visible={visible}
-                onDismiss={hideModal}
-                contentContainerStyle={containerStyle}
-              >
-                <Text>You have 10 points Left</Text>
-              </Modal>
-            </Portal>
+      <GestureHandlerRootView style={{ height: "100%" }}>
+        <NavigationContainer>
+          {flag ? (
+            <>
+              {/* <BottomSheet isVisible={bflag} onClose={() => setbFlag(false)} /> */}
 
-            <Stack.Navigator
-              initialRouteName="Home"
-              screenOptions={{
-                tabBarStyle: {
-                  backgroundColor: "#171717",
-                  borderTopWidth: 1,
-                  height: 53,
-                  borderTopColor: "#282828",
-                },
-                tabBarActiveTintColor: "#40e6b5",
-                headerTitle: "Shera Ai",
-                headerTintColor: "#FFFFFF",
-                headerStyle: styles.headerStyle,
-                headerTitleStyle: styles.headerTitleStyle,
-                headerLeft: customHeaderLeft,
-                headerRight: () => customHeaderRight({ showModal }),
-              }}
-            >
-              <Stack.Screen name="Home" component={Home} />
-              <Stack.Screen name="ChatScreen" component={ChatScreen} />
-            </Stack.Navigator>
-          </>
-        ) : (
-          <>
-            <OnBoarding setFlag={setFlag} />
-          </>
-        )}
-      </NavigationContainer>
+              <Portal>
+                <Modal
+                  visible={visible}
+                  onDismiss={hideModal}
+                  contentContainerStyle={containerStyle}
+                >
+                  <Animated.View>
+                    <Text>You have 10 points Left</Text>
+                  </Animated.View>
+                </Modal>
+              </Portal>
+
+              <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                  tabBarStyle: {
+                    backgroundColor: "#171717",
+                    borderTopWidth: 1,
+                    height: 53,
+                    borderTopColor: "#282828",
+                  },
+                  tabBarActiveTintColor: "#40e6b5",
+                  headerTitle: "Shera Ai",
+                  headerTintColor: "#FFFFFF",
+                  headerStyle: styles.headerStyle,
+                  headerTitleStyle: styles.headerTitleStyle,
+                  headerLeft: customHeaderLeft,
+                  headerRight: () =>
+                    customHeaderRight({
+                      showModal,
+                      showBottomSheet,
+                    }),
+                }}
+              >
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="ChatScreen" component={ChatScreen} />
+              </Stack.Navigator>
+
+              <BottomSheet
+                snapPoints={snapPoints}
+                ref={bottomSheetModalRef}
+                index={-1}
+                onChange={handleSheetChanges}
+                enableHandlePanningGesture={true}
+                animateOnMount={true}
+                enablePanDownToClose={true}
+                topInset={StatusBar.currentHeight}
+                handleStyle={{ backgroundColor: "black" }}
+                handleIndicatorStyle={{ backgroundColor: "gray" }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "black",
+                    height: "100%",
+                    width: "100%",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: "white",
+                      alignSelf: "center",
+                    }}
+                  >
+                    Awesome
+                  </Text>
+                </View>
+              </BottomSheet>
+            </>
+          ) : (
+            <>
+              <OnBoarding setFlag={setFlag} />
+            </>
+          )}
+        </NavigationContainer>
+      </GestureHandlerRootView>
     </PaperProvider>
   );
 }
@@ -210,5 +281,38 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
+  },
+  contentContainer: {
+    paddingTop: 2,
+    flex: 1,
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "green",
+    alignItems: "center",
+    height: 60,
+    justifyContent: "center",
+  },
+  closeButton: {
+    marginTop: 16,
+    backgroundColor: "red",
+    borderRadius: 8,
+    flex: 1,
+    padding: 12,
+  },
+  closeButtonText: {
+    backgroundColor: "#fff",
+    alignSelf: "center",
+    fontSize: 70,
+    fontWeight: "bold",
+  },
+  bottomSheetContainer: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "flex-start",
+    backgroundColor: "black",
+  },
+  buttonText: {
+    color: "white",
   },
 });
